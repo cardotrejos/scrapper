aa92b6599a3ae2d001e24253be47c659a875083c
Ricardo <cardotrejos@gmail.com>
2020-11-12 22:06:49 -0500

    setup heroku build

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask, jsonify
 from web_scraper import get_data
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def welcome():
     results = get_data()
     return jsonify(results)
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=os.getenv('PORT'))
+      app.run(threaded=True, port=5000)
