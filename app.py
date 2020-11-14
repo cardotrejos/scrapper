6eb9cc4f88b52611506f4357c3770aa4cdaee35d
Ricardo <cardotrejos@gmail.com>
2020-11-14 16:35:45 -0500

    setting cors

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask, jsonify
 from web_scraper import get_data
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET'])
 def welcome():
